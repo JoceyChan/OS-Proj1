e32434b72fff14f969749eed0783349bb774b08d
Jocey Chan <joceychan331718@gmail.com>
2021-10-27 15:48:37 -0700

    formaating class

--- nachos/threads/Communicator.java
+++ nachos/threads/Communicator.java
@@ -94,68 +94,77 @@ class CommSelfTester {
  * Test with 1 listener then 1 speaker.
  */
 public static void selfTest1() {
-KThread listener1 = new KThread(listenRun);
-listener1.setName("listener1");
-listener1.fork();
-KThread speaker1 = new KThread(speakerRun);
-speaker1.setName("speaker1");
-speaker1.fork();
+	KThread listener1 = new KThread(listenRun);
+	listener1.setName("listener1");
+	listener1.fork();
+	KThread speaker1 = new KThread(speakerRun);
+	speaker1.setName("speaker1");
+	speaker1.fork();
 } // selfTest1()
 /**
  * Test with 1 speaker then 1 listener.
  */
 public static void selfTest2() {
-KThread speaker1 = new KThread(speakerRun);
-speaker1.setName("speaker1");
-speaker1.fork();
-KThread listener1 = new KThread(listenRun);
-listener1.setName("listener1");
-listener1.fork();
+	KThread speaker1 = new KThread(speakerRun);
+	speaker1.setName("speaker1");
+	speaker1.fork();
+	
+	KThread listener1 = new KThread(listenRun);
+	listener1.setName("listener1");
+	listener1.fork();
 } // selfTest2()
 /**
  * Test with 2 speakers and 2 listeners intermixed.
  */
 public static void selfTest3() {
-KThread speaker1 = new KThread(speakerRun);
-speaker1.setName("speaker1");
-speaker1.fork();
-KThread listener1 = new KThread(listenRun);
-listener1.setName("listener1");
-listener1.fork();
-KThread speaker2 = new KThread(speakerRun);
-speaker2.setName("speaker2");
-speaker2.fork();
-KThread listener2 = new KThread(listenRun);
-listener2.setName("listener2");
-listener2.fork();
+	KThread speaker1 = new KThread(speakerRun);
+	speaker1.setName("speaker1");
+	speaker1.fork();
+	
+	KThread listener1 = new KThread(listenRun);
+	listener1.setName("listener1");
+	listener1.fork();
+	
+	KThread speaker2 = new KThread(speakerRun);
+	speaker2.setName("speaker2");
+	speaker2.fork();
+	
+	KThread listener2 = new KThread(listenRun);
+	listener2.setName("listener2");
+	listener2.fork();
 } // selfTest3()
 /**
  * Second test with 2 speakers and 2 listeners intermixed.
  */
 public static void selfTest4() {
-KThread speaker1 = new KThread(speakerRun);
-speaker1.setName("speaker1");
-speaker1.fork();
-KThread speaker2 = new KThread(speakerRun);
-speaker2.setName("speaker2");
-speaker2.fork();
-KThread listener1 = new KThread(listenRun);
-listener1.setName("listener1");
-listener1.fork();
-KThread listener2 = new KThread(listenRun);
-listener2.setName("listener2");
-listener2.fork();
+	KThread speaker1 = new KThread(speakerRun);
+	speaker1.setName("speaker1");
+	speaker1.fork();
+	
+	KThread speaker2 = new KThread(speakerRun);
+	speaker2.setName("speaker2");
+	speaker2.fork();
+	
+	KThread listener1 = new KThread(listenRun);
+	listener1.setName("listener1");
+	listener1.fork();
+	
+	KThread listener2 = new KThread(listenRun);
+	listener2.setName("listener2");
+	listener2.fork();
 } // selfTest4()
 /**
  * Stress test with 100 speakers and 100 listeners intermixed.
  */
 public static void selfTest5() {
-for (int i = 0; i < 100; i++) {
-new KThread(speakerRun).setName("Speaker " + 
-Integer.toString(i)).fork();
-new KThread(listenRun).setName("Listen " + 
-Integer.toString(i)).fork();
-}
+	
+	for (int i = 0; i < 100; i++) {
+		new KThread(speakerRun).setName("Speaker " + 
+		Integer.toString(i)).fork();
+		
+		new KThread(listenRun).setName("Listen " + 
+		Integer.toString(i)).fork();
+	}
 } // selfTest5()
 /**
  * Function to run inside Runnable object listenRun. Uses the function listen
@@ -167,10 +176,10 @@ NACHOS
  * README for info on how to run in debug mode.
  */
 static void listenFunction() {
-Lib.debug(dbgThread, "Thread " + KThread.currentThread().getName() + " 
-is about to listen");
-Lib.debug(dbgThread, "Thread " + KThread.currentThread().getName() + " 
-got value " + myComm.listen());
+	Lib.debug(dbgThread, "Thread " + KThread.currentThread().getName() + " 
+	is about to listen");
+	Lib.debug(dbgThread, "Thread " + KThread.currentThread().getName() + " 
+	got value " + myComm.listen());
 } // listenFunction()
 /**
  * Function to run inside Runnable object speakerRun. Uses the function 
@@ -181,11 +190,11 @@ listen
  * README for info on how to run in debug mode.
  */
 static void speakFunction() {
-Lib.debug(dbgThread, "Thread " + KThread.currentThread().getName() + " 
-is about to speak");
-myComm.speak(myWordCount++);
-Lib.debug(dbgThread, "Thread " + KThread.currentThread().getName() + " 
-has spoken");
+	Lib.debug(dbgThread, "Thread " + KThread.currentThread().getName() + " 
+	is about to speak");
+	myComm.speak(myWordCount++);
+	Lib.debug(dbgThread, "Thread " + KThread.currentThread().getName() + " 
+	has spoken");
 } // speakFunction()
 /**
  * Wraps listenFunction inside a Runnable object so threads can be generated 
@@ -193,9 +202,9 @@ for
  * testing.
  */
 private static Runnable listenRun = new Runnable() {
-public void run() {
-listenFunction();
-}
+	public void run() {
+		listenFunction();
+	}
 }; // runnable listenRun
 /**
  * Wraps speakFunction inside a Runnable object so threads can be generated 
@@ -203,9 +212,9 @@ for
  * testing.
  */
 private static Runnable speakerRun = new Runnable() {
-public void run() {
-speakFunction();
-}
+	public void run() {
+		speakFunction();
+	}
 }; // Runnable speakerRun
 // dbgThread = 't' variable needed for debug output
 private static final char dbgThread = 't';
